0289ea5b69bf0e652d41cf73edd0d777dcbc3b75
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-03-30 00:25:30 +0330

    details about slider created and used

--- Components/Data/Data.js
+++ Components/Data/Data.js
@@ -6,4 +6,39 @@ let dataHeaderBoxes = [
   { id: 5, icon: "ri-database-2-line", title: "اطلاعات ذخیره شده" },
 ];
 
-export {dataHeaderBoxes}
+let aboutSliderData=[
+  {
+    id:1,
+    img:"/images/png/client-1.png"
+  },
+  {
+    id:2,
+    img:"/images/png/client-2.png"
+  },
+  {
+    id:3,
+    img:"/images/png/client-3.png"
+  },
+  {
+    id:4,
+    img:"/images/png/client-4.png"
+  },
+  {
+    id:5,
+    img:"/images/png/client-5.png"
+  },
+  {
+    id:6,
+    img:"/images/png/client-6.png"
+  },
+  {
+    id:7,
+    img:"/images/png/client-7.png"
+  },
+  {
+    id:8,
+    img:"/images/png/client-8.png"
+  },
+]
+
+export {dataHeaderBoxes,aboutSliderData}
