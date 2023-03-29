35b2a5ffa2431cc70992ef2183c3f086d5b237d8
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-03-29 10:50:56 +0330

    details index file changed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Header from '@/Components/Header/Header'
+import Head from "next/head";
+import Header from "@/Components/Header/Header";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -16,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      
     </>
-  )
+  );
 }
