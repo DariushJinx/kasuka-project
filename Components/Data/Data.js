badd5b2f7afe693e5566cc032120ca5f4a165709
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-03-29 10:48:27 +0330

    added data for dynamic boxes

---
+++ Components/Data/Data.js
@@ -0,0 +1,9 @@
+let dataHeaderBoxes = [
+  { id: 1, icon: "ri-store-line", title: "متن نمایشی" },
+  { id: 2, icon: "ri-bar-chart-box-line", title: "نمودار کیفی" },
+  { id: 3, icon: "ri-calendar-todo-fill", title: "نمونه کارها" },
+  { id: 4, icon: "ri-paint-brush-line", title: "عملکردهای پایانی" },
+  { id: 5, icon: "ri-database-2-line", title: "اطلاعات ذخیره شده" },
+];
+
+export {dataHeaderBoxes}
