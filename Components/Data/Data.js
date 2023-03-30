84acf40caa32b8061e35dccc1d67b4a7a42a628e
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-03-30 17:12:11 +0330

    data file updated

--- Components/Data/Data.js
+++ Components/Data/Data.js
@@ -6,39 +6,105 @@ let dataHeaderBoxes = [
   { id: 5, icon: "ri-database-2-line", title: "اطلاعات ذخیره شده" },
 ];
 
-let aboutSliderData=[
+let aboutSliderData = [
   {
-    id:1,
-    img:"/images/png/client-1.png"
+    id: 1,
+    img: "/images/png/client-1.png",
   },
   {
-    id:2,
-    img:"/images/png/client-2.png"
+    id: 2,
+    img: "/images/png/client-2.png",
   },
   {
-    id:3,
-    img:"/images/png/client-3.png"
+    id: 3,
+    img: "/images/png/client-3.png",
   },
   {
-    id:4,
-    img:"/images/png/client-4.png"
+    id: 4,
+    img: "/images/png/client-4.png",
   },
   {
-    id:5,
-    img:"/images/png/client-5.png"
+    id: 5,
+    img: "/images/png/client-5.png",
   },
   {
-    id:6,
-    img:"/images/png/client-6.png"
+    id: 6,
+    img: "/images/png/client-6.png",
   },
   {
-    id:7,
-    img:"/images/png/client-7.png"
+    id: 7,
+    img: "/images/png/client-7.png",
   },
   {
-    id:8,
-    img:"/images/png/client-8.png"
+    id: 8,
+    img: "/images/png/client-8.png",
   },
-]
+];
 
-export {dataHeaderBoxes,aboutSliderData}
+let advertiseData = [
+  {
+    id: 1,
+    title: "تبلیغ سختی است",
+    par: "نتایج یا در نتیجه هر یک از این اهداف اشتغال حاصل می شوند",
+    icon: "ri-pages-line",
+  },
+  {
+    id: 2,
+    title: "کدام یک از اینهاست",
+    par: "کسانی که هوس سیاهان می کنند نمی بینند ، آنها مقصر هستند که خدمات خود را رها کرده اند",
+    icon: "ri-vip-diamond-line",
+  },
+  {
+    id: 3,
+    title: "یا کور شده است",
+    par: "یا می پذیرد یا زمانی که هیچ کس از همه اجتناب نمی کند. همه دردهایی که آنها را بیشتر می کند",
+    icon: "ri-gallery-line",
+  },
+  {
+    id: 4,
+    title: "حقیقت مبارک",
+    par: "آنها بدون تحمل حقیقت در زمان ستایش زندگی از هیچ چیزی پیروی نمی کنند",
+    icon: "ri-shield-line",
+  },
+];
+
+let servicesData = [
+  {
+    id: 1,
+    icon: "ri-dribbble-line",
+    title: "لورم اپیسوم",
+    par: "برای آرامش بخشیدن و لذت بردن از مفاسد ، چه دردها و چه دردسرهایی را دریافت خواهند کرد",
+  },
+  {
+    id: 2,
+    icon: "ri-file-3-line",
+    title: "اما همانطور که مشاهده می کنید",
+    par: "آنها استثنائاتی هستند که به دنبال سیاه پوستان هستند ، آنها نمی بینند ، آنها کسانی هستند که در خدمات خود مقصر هستند",
+  },
+  {
+    id: 3,
+    icon: "ri-edit-circle-line",
+    title: "دردهای بزرگ",
+    par: "آنها استثنائاتی هستند که به دنبال سیاه پوستان هستند ، آنها نمی بینند ، آنها کسانی هستند که در خدمات خود مقصر هستند",
+  },
+  {
+    id: 4,
+    icon: "ri-earth-line",
+    title: "هیچکس دیگر",
+    par: "اما در واقع ما هم آنها را متهم می کنیم و هم با یک نفرت عادلانه شایسته چاپلوسی هستیم",
+  },
+  {
+    id: 5,
+    icon: "ri-tv-2-line",
+    title: "لولا را پاک کنید",
+    par: "آنها استثنائاتی هستند که به دنبال سیاه پوستان هستند ، آنها نمی بینند ، آنها کسانی هستند که در خدمات خود مقصر هستند",
+  },
+  {
+    id: 6,
+    icon: "ri-home-gear-line",
+    title: "دیوا دان",
+    par: "روشها یا تلاشهای ما علاوه بر این ، خطا فرار می کند ، بگذار کوچکتر عاقل باشد",
+  },
+];
+
+export { dataHeaderBoxes, aboutSliderData, advertiseData, servicesData };
