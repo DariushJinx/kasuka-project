fc2fc88e8735384fb336d3e1fa1954f039327686
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-03-30 16:11:28 +0330

    advertise section added

---
+++ Components/Advertise/Advertise.js
@@ -0,0 +1,51 @@
+import styles from "./Advertise.module.css";
+import { advertiseData } from "../Data/Data";
+import Image from "next/image";
+
+export default function Advertise() {
+  return (
+    <div className="advertise my-5">
+      <div className="container">
+        <div className="row">
+          <div className="advertise__image col-lg-6">
+            <Image
+              src="/images/jpeg/advertise.jpg"
+              alt="advertise"
+              width={636}
+              height={477}
+              quality={100}
+              className="rounded-4"
+            />
+          </div>
+
+          <div className="advertise__details col-lg-6">
+            <div className="row">
+              {advertiseData.map((info) => (
+                <div
+                  className="advertise__details_item col-lg-12 d-flex align-items-center my-lg-4"
+                  key={info.id}
+                >
+                  <div className="advertise__details_item_social ms-3">
+                    <i
+                      class={`${info.icon} ${styles.advertise__details_item_social_icon}`}
+                    ></i>
+                  </div>
+                  <div className="advertise__details_item_desc">
+                    <h3
+                      className={`${styles.advertise__details_item_desc_title}`}
+                    >
+                      {info.title}
+                    </h3>
+                    <p className={`${styles.advertise__details_item_desc_par}`}>
+                      {info.par}{" "}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
