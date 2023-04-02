4ddb44a4cbe13736365885424698cc458451a68a
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-04-02 11:05:59 +0330

    menu file details updated

--- Components/Menu/Menu.js
+++ Components/Menu/Menu.js
@@ -1,7 +1,6 @@
-import styles from "./Menu.module.css";
 import { useState } from "react";
 import CategoryMenu from "../CategoryMenu/CategoryMenu";
-import {dataPortfolio} from "../Data/Data";
+import { dataPortfolio } from "../Data/Data";
 import MenuDetails from "../MenuDetails/MenuDetails";
 import Title from "../Title/Title";
 let allCategories = [
@@ -11,26 +10,26 @@ let allCategories = [
 
 export default function Menu() {
   let [categories, setCategories] = useState(allCategories);
-  let [allMenus,setAllMenu] = useState(dataPortfolio)
+  let [allMenus, setAllMenu] = useState(dataPortfolio);
 
   let filterMenus = (category) => {
-    if(category === 'همه'){
-        setAllMenu(dataPortfolio)
-        return
+    if (category === "همه") {
+      setAllMenu(dataPortfolio);
+      return;
     }
 
-    let filteredMenus = dataPortfolio.filter(data => data.category === category)
+    let filteredMenus = dataPortfolio.filter(
+      (data) => data.category === category
+    );
 
-    setAllMenu(filteredMenus)
-
-  }
+    setAllMenu(filteredMenus);
+  };
 
   return (
     <div className="menu my-5">
       <div className="container">
         <div className="row">
-
-        <Title title="خدمات" par="خدمات ما را بررسی کنید" />
+          <Title title="خدمات" par="خدمات ما را بررسی کنید" />
 
           <CategoryMenu categories={categories} filterMenus={filterMenus} />
           <MenuDetails allMenus={allMenus} />
